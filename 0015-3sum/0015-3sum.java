6a9bbd5ee4f010af68ccc8747ef49468b2403655
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2025-09-29 00:35:27 +0530

    Time: 28 ms (81.35%), Space: 51.4 MB (70.07%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-         Set<List<Integer>> st = new HashSet<>();
-         int n = nums.length;
+       
 
-        for (int i = 0; i < n; i++) {
-            Set<Integer> hashset = new HashSet<>();
-            for (int j = i + 1; j < n; j++) {
-                
-                int third = -(nums[i] + nums[j]);
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
-                
-                if (hashset.contains(third)) {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], third);
-                    // temp.sort(null);
-                    // st.add(temp);
-                    List<Integer> sortedTemp = new ArrayList<>(temp);
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i]== nums[i-1]){
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
 
-                        Collections.sort(sortedTemp);
-                        st.add(sortedTemp);
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum >0){
+                    k--;
                 }
-                hashset.add(nums[j]);
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+
+                     while(j<k && nums[j]==nums[j-1]){
+                    j++;
+                }
+                  while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                }
+               
             }
         }
-
-      
-        List<List<Integer>> ans = new ArrayList<>(st);
-        return ans;
+        return result;
         
     }
 }
