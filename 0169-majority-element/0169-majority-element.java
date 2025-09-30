3d4e61eb86654a8677d95ae08bd28a739db96575
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2025-09-30 23:56:52 +0530

    Time: 2 ms (74.19%), Space: 53.6 MB (8.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,41 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       HashMap<Integer, Integer> counts = new HashMap<>();
+    //    HashMap<Integer, Integer> counts = new HashMap<>();
 
-        // Count occurrences of each element
-        for (int num : nums) {
-            counts.put(num, counts.getOrDefault(num, 0) + 1);
+    //     // Count occurrences of each element
+    //     for (int num : nums) {
+    //         counts.put(num, counts.getOrDefault(num, 0) + 1);
+    //     }
+
+    //     int majorityCount = nums.length / 2;
+
+    //     // Find the element with count > majorityCount
+    //     for (int num : nums) {
+    //         if (counts.get(num) > majorityCount) {
+    //             return num;
+    //         }
+    //     }
+    //     return -1;
+
+    int element = nums[0];
+    int count=0;
+
+    for(int i =0;i<nums.length;i++){
+        if(element == nums[i]){
+            count++;
+        }
+        else{
+            count--;
+        }
+        if(count==0 || count<0){
+            element = nums[i];
+            count++;
+
         }
 
-        int majorityCount = nums.length / 2;
 
-        // Find the element with count > majorityCount
-        for (int num : nums) {
-            if (counts.get(num) > majorityCount) {
-                return num;
-            }
-        }
-        return -1;
+    }
+    return element;
         
     }
 }
