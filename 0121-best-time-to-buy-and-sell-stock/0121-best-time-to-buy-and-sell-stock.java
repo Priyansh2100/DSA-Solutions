086de24d577efdb9d8052949d34b9083631d3f1a
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2025-11-26 23:56:09 +0530

    Time: 2 ms (62.75%), Space: 94.3 MB (30.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int bestbuy = prices[0];
-        int maxprofit =0;
-        for( int i =1;i<prices.length;i++){
 
-            if(prices[i]>bestbuy){
-             maxprofit = Math.max(maxprofit,(prices[i]-bestbuy));
+        // store the best (minimum) price to buy so far
+        int bestbuy = prices[0];  
+
+        // store maximum profit so far
+        int maxprofit = 0;  
+
+        // iterate through prices from day 1
+        for (int i = 1; i < prices.length; i++) {
+
+            // if current price is greater than bestbuy, calculate profit
+            if (prices[i] > bestbuy) {
+                maxprofit = Math.max(maxprofit, (prices[i] - bestbuy));
             }
 
-            bestbuy = Math.min(bestbuy,prices[i]);
-
+            // update bestbuy if we find a lower price
+            bestbuy = Math.min(bestbuy, prices[i]);
         }
+
+        // return maximum profit after checking all days
         return maxprofit;
     }
 }
