39022748bf22306954e3f0146efa114fab33cf55
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2025-12-04 23:52:56 +0530

    Time: 2 ms (84.76%), Space: 47.7 MB (35.67%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int i = 0 , k=0;
+          int n = nums.length;
+         // Store the length of the array
+        // for (i=0;i<n;i++)
+        // {
+        //     if(nums[i]!=0)
+        //     {
+        //         nums[k]=nums[i];
+        //         k++;
+        //     }
+
+        // }
+
+        // for (int j=k;j<n;j++)
+        // {
+        // nums[j]=0;
+
+        // }
+     int j=-1;
+        for( i=0;i<n;i++){
+            if(nums[i]==0){
+              j =i;
+              break;
+            }
+        }
+          // If there is no zero, array already fine
+        if (j == -1) {
+            return;
+        }
+
+        for( i = j+1;i<n;i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i]= nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
