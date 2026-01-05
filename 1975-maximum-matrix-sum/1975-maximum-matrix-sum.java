4d85f9d67e1fbc415866a0d9787e4026d4ce12d0
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-01-05 23:46:08 +0530

    Time: 6 ms (73.94%), Space: 68.1 MB (34.55%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -1,52 +1,25 @@
-
-// Done  my own 
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        long sum =0;
-        int smallest =Math.abs(matrix[0][0]);
-        int count =0;
+         long sum = 0;
+        int smallest = Integer.MAX_VALUE;
+        int count = 0;
 
-        for(int i=0;i<matrix.length;i++){
-            int arr[] = matrix[i];
-            for(int j =0;j<arr.length;j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int val = matrix[i][j];
 
-                if(arr[j]<0){
-                    count++;
-                }
-                int temp = Math.abs(arr[j]);
-                smallest = Math.min(smallest,temp);
-                
+                if (val < 0) count++;
+
+                int abs = Math.abs(val);
+                smallest = Math.min(smallest, abs);
+                sum += abs;
             }
         }
 
-        if(count % 2 ==0){
-            for(int i=0;i<matrix.length;i++){
-            int arr[] = matrix[i];
-            for(int j =0;j<arr.length;j++){
-                if(arr[j]<0){
-                    arr[j] = -(arr[j]);
-                }
-                sum = sum + arr[j];
-            }
-        }
-        }
-        else{
-             for(int i=0;i<matrix.length;i++){
-            int arr[] = matrix[i];
-            for(int j =0;j<arr.length;j++){
-                if(arr[j]<0){
-                    arr[j] = -(arr[j]);
-                }
-                sum = sum + arr[j];
-            }
-        }
-    
-       sum = sum -(long)smallest-(long)smallest;
-
+        if (count % 2 != 0) {
+            sum -= 2L * smallest;
         }
 
-return sum;
-
-        
+        return sum;
     }
 }
