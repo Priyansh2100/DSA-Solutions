3365978f3cc659be180a60dc5871ffdb544db601
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-03-15 21:10:46 +0530

    Time: 44 ms (17.16%), Space: 56.1 MB (93.24%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,34 +1,21 @@
 public class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-
-        int oddCount = 0;
         int count = 0;
-        int result = 0;
+        int currSum = 0;
+        map.put(currSum, 1);
 
-        int i = 0;
-        int j = 0;
+        for (int i = 0; i < n; i++) {
+            currSum += (nums[i] % 2); // if odd - 1, even - 0
 
-        // Best example to understand the use of count variable - {2, 1, 2, 1}, output : 6
-        while (j < n) {
-            if (nums[j] % 2 != 0) { // ODD
-                oddCount++;
-                count = 0;
+            if (map.containsKey(currSum - k)) {
+                count += map.get(currSum - k);
             }
 
-            while (oddCount == k) {
-                count++;
-
-                if (i < n && nums[i] % 2 == 1) { // ODD
-                    oddCount--;
-                }
-                i++;
-            }
-
-            result += count;
-            j++;
+            map.put(currSum, map.getOrDefault(currSum, 0) + 1);
         }
 
-        return result;
+        return count;
     }
 }
