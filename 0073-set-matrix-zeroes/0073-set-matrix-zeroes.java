d2d728d587f48daf02f9ea72b7b74461369cbfaf
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-05-10 23:52:08 +0530

    Time: 1 ms (98.24%), Space: 47.8 MB (25.49%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,8 +2,8 @@ class Solution {
     public void setZeroes(int[][] matrix) {
          int m = matrix.length, n = matrix[0].length;
 
-        int[] row = new int[m]; // to mark zero rows
-        int[] col = new int[n]; // to mark zero cols
+        int[] row = new int[m]; 
+        int[] col = new int[n]; 
 
         // Step 1: Mark rows & cols that have a 0
         for (int i = 0; i < m; i++) {
