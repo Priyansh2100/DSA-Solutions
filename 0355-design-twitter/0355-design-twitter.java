83016c5c1a9e4167cc1f5543a2973af0d8b9ba6a
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-05-21 22:46:07 +0530

    Time: 36 ms (34.57%), Space: 56.4 MB (64.27%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,38 +1,95 @@
 class Twitter {
-    private static int timestamp = 0;
-    private Map<Integer, List<int[]>> tweets = new HashMap<>();
-    private Map<Integer, Set<Integer>> followMap = new HashMap<>();
 
+    Map<Integer, Set<Integer>> followMap;
+
+    Map<Integer, List<int[]>> tweetMap;
+
+    int time;
+
+    public Twitter() {
+
+        followMap = new HashMap<>();
+        tweetMap = new HashMap<>();
+
+        time = 0;        
+    }
+    
     public void postTweet(int userId, int tweetId) {
-        tweets.putIfAbsent(userId, new ArrayList<>());
-        tweets.get(userId).add(new int[]{timestamp++, tweetId});
-    }
 
+        tweetMap.putIfAbsent(userId, new ArrayList<>());
+
+        tweetMap.get(userId).add(new int[]{time++, tweetId});
+        
+    }
+    
     public List<Integer> getNewsFeed(int userId) {
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        Set<Integer> followees = followMap.getOrDefault(userId, new HashSet<>());
-        followees.add(userId);
 
-        for (int followee : followees) {
-            List<int[]> tweetList = tweets.getOrDefault(followee, new ArrayList<>());
-            maxHeap.addAll(tweetList);
+         List<Integer> result = new ArrayList<>();
+
+        // Max Heap based on latest time
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+            (a, b) -> b[0] - a[0]
+        );
+
+        // add user's own tweets
+        if (tweetMap.containsKey(userId)) {
+
+            List<int[]> tweets = tweetMap.get(userId);
+
+            for (int[] tweet : tweets) {
+                maxHeap.offer(tweet);
+            }
         }
 
-        List<Integer> res = new ArrayList<>();
-        while (!maxHeap.isEmpty() && res.size() < 10) {
-            res.add(maxHeap.poll()[1]);
+        // add followees tweets
+        if (followMap.containsKey(userId)) {
+
+            for (int followee : followMap.get(userId)) {
+
+                if (tweetMap.containsKey(followee)) {
+
+                    List<int[]> tweets = tweetMap.get(followee);
+
+                    for (int[] tweet : tweets) {
+                        maxHeap.offer(tweet);
+                    }
+                }
+            }
         }
-        return res;
+
+        // get top 10 latest tweets
+        while (!maxHeap.isEmpty() && result.size() < 10) {
+
+            result.add(maxHeap.poll()[1]);
+        }
+
+        return result;
+        
     }
-
+    
     public void follow(int followerId, int followeeId) {
-        followMap.putIfAbsent(followerId, new HashSet<>());
-        followMap.get(followerId).add(followeeId);
-    }
 
+        followMap.putIfAbsent(followerId, new HashSet<>());
+
+        followMap.get(followerId).add(followeeId);
+        
+    }
+    
     public void unfollow(int followerId, int followeeId) {
-        if (followMap.containsKey(followerId)) {
+
+        if(followMap.containsKey(followerId)){
+
             followMap.get(followerId).remove(followeeId);
         }
+        
     }
 }
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
