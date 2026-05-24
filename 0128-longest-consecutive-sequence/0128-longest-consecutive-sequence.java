593bc6b1cb08817738fa712b8b8fc878979cea30
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-05-24 23:55:45 +0530

    Time: 43 ms (11.36%), Space: 94.3 MB (74.39%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,53 +1,39 @@
 class Solution {
+
+     public static int findMax(HashMap<Integer,Boolean> map, int k){
+
+            int ans =0;
+
+            while(map.containsKey(k)){
+                ans++;
+                k++;
+            }
+            return ans;
+        }
     public int longestConsecutive(int[] nums) {
-        // if(nums.length ==0){
-        //     return 0;
-        // }
-        // int longest = 1;
 
-        // Arrays.sort(nums);
-        // int count = 1;
-        // for(int i=0;i<nums.length-1;i++){
-        //     if(nums[i] == nums[i+1]){
-        //         continue;
-        //     }
-        //     if(nums[i]+1==nums[i+1]){
-        //         count = count + 1;
-                
-        //     }
-        //     else{
-        //       longest = Math.max(count,longest);
-        //       count=1;
-        //     }
-        // }
-        // return Math.max(count, longest);
-        int n = nums.length;
-        if (n == 0)
-            return 0;
+        HashMap<Integer, Boolean> map = new HashMap<>();
 
-        int longest = 1;
-        Set<Integer> set = new HashSet<>();
-
-        // put all the array elements into set
-        for (int i = 0; i < n; i++) {
-            set.add(nums[i]);
+        for(int num : nums){
+            map.put(num,true);
         }
 
-        // Find the longest sequence
-        for (int it : set) {
-            // if 'it' is a starting number
-            if (!set.contains(it - 1)) {
-                // find consecutive numbers
-                int cnt = 1;
-                int x = it;
-                while (set.contains(x + 1)) {
-                    x = x + 1;
-                    cnt = cnt + 1;
-                }
-                longest = Math.max(longest, cnt);
+        for(int num : nums){
+            if(map.containsKey(num-1)){
+                map.put(num,false);
             }
         }
-        return longest;
+
+        int max =0;
+        for(Integer i : map.keySet()){
+
+            if(map.get(i) == true){
+                max = Math.max(max, findMax(map,i));
+            }
+
+        }
+
+       return max;
         
     }
 }
