3dfe8f7f72b3c923674cbbb54d22fa8bc9487638
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-07-25 23:26:07 +0530

    Time: 161 ms (23.92%), Space: 266.4 MB (10.54%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,54 +1,45 @@
-import java.util.*;
-
 class Solution {
     public int[] getOrder(int[][] tasks) {
 
         int n = tasks.length;
 
-        // store tasks with index
-        int[][] arr = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = tasks[i][0]; // enqueueTime
-            arr[i][1] = tasks[i][1]; // processingTime
-            arr[i][2] = i;           // index
+        int [][]arr = new int[n][3];
+
+        for(int i =0;i<n;i++){
+            
+            arr[i][0] = tasks[i][0];
+            arr[i][1] = tasks[i][1];
+            arr[i][2] = i;
         }
 
-        // sort by enqueueTime
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        Arrays.sort(arr,(a,b)->{ return a[0]-b[0]; });
 
-        // min heap (processingTime, index)
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> {
-                if (a[1] != b[1]) return a[1] - b[1];
-                return a[2] - b[2];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+
+            if(a[1]!=b[1]){
+              return  a[1]-b[1];
             }
-        );
+            return a[2]-b[2];
+        });
 
-        int time = 0;
-        int i = 0;
-        int idx = 0;
-        int[] result = new int[n];
+        int i =0;
+        int timer =0;
+        int idx =0;
+       int[] result = new int[n];
 
-        // step 4: simulation
-        while (i < n || !pq.isEmpty()) {
+        while(!pq.isEmpty() || i<n){
 
-            // if cpu idle and next task not yet arrived
-            if (pq.isEmpty() && time < arr[i][0]) {
-                time = arr[i][0];
-            }
+            if (pq.isEmpty() && timer < arr[i][0]) { timer = arr[i][0]; }
 
-            // add all available tasks
-            while (i < n && arr[i][0] <= time) {
-                pq.offer(arr[i]);
-                i++;
-            }
+            while (i < n && arr[i][0] <= timer) { pq.offer(arr[i]); i++; }
 
-            // process next task
-            int[] task = pq.poll();
-            result[idx++] = task[2];
-            time += task[1];
+
+            int[] temp = pq.poll();
+            result[idx++] = temp[2];
+            timer += temp[1];
         }
 
         return result;
+        
     }
 }
