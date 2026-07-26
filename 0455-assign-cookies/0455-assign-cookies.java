54968056bb7fab7b6c9a756ed83bb358027df520
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-07-26 17:43:11 +0530

    Time: 15 ms (60.33%), Space: 51.8 MB (6.54%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,23 +1,22 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-         int gLen = g.length; // Greed Factor for students
-        int sLen = s.length; // Cookies
-
-        // Sort the array so the we can gready select the least required cookies for ith children with greedFactor greater than or equals to its requirement.
+        
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int res = 0;
+        int m = g.length;
+        int n = s.length;
 
-        int ith_greedFactor = 0;
-        int jth_cookies = 0;
-        while(ith_greedFactor < gLen && jth_cookies < sLen){
-            if(s[jth_cookies] >= g[ith_greedFactor]){ // Given Condition
-                ith_greedFactor++; 
-                res++;
+        int i =0;
+        int j =0;
+
+        while(i<m && j<n){
+            if(g[i]<=s[j]){
+                i++;
             }
-            jth_cookies++;
+            j++;
         }
-        return res;
+
+        return i;
     }
 }
