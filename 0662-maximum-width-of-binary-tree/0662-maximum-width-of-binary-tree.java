f89c3962c5a01d2c22dbd36cab17bdb3353cbc00
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-08-26 10:36:18 +0530

    Time: 2 ms (42.07%), Space: 45.2 MB (33.43%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -15,32 +15,38 @@
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
-        q.offer(new Pair<>(root, 0));
-        int maxWidth = 0;
 
-        while (!q.isEmpty()) {
+        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
+
+        q.offer(new Pair<>(root,0));
+        int maxWidth =0;
+
+        while(q.size()>0){
+
             int currLevelSize = q.size();
             int stIdx = q.peek().getValue();
-            int endIdx = q.peek().getValue();
+            int endIdx =  q.peek().getValue();
 
-            for (int i = 0; i < currLevelSize; i++) {
-                Pair<TreeNode, Integer> curr = q.poll();
-                TreeNode node = curr.getKey();
-                int idx = curr.getValue();
-                endIdx = idx;
+            for(int i =0;i<currLevelSize;i++){
+             Pair<TreeNode,Integer> curr = q.poll();
+             TreeNode node = curr.getKey();
+             int idx = curr.getValue();
 
-                if (node.left != null) {
-                    q.offer(new Pair<>(node.left, idx * 2 + 1));
-                }
-                if (node.right != null) {
-                    q.offer(new Pair<>(node.right, idx * 2 + 2));
-                }
+             endIdx = idx;
+
+             if(node.left!=null){
+                q.offer(new Pair<>(node.left,idx*2+1));
+             }
+             if(node.right!=null){
+                q.offer(new Pair<>(node.right,idx*2+2));
+             }
+
             }
 
-            maxWidth = Math.max(maxWidth, endIdx - stIdx + 1);
-        }
+            maxWidth = Math.max(maxWidth,endIdx - stIdx +1);
 
+        }
         return maxWidth;
+        
     }
 }
