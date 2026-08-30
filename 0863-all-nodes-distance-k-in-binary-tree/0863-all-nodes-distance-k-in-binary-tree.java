a99cab37d1c3c7bed498f920d2f79106fbbf6023
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-08-31 00:18:16 +0530

    Time: 12 ms (83.31%), Space: 43.9 MB (76%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -1,59 +1,61 @@
-import java.util.*;
 
 class Solution {
 
-    Map<TreeNode, TreeNode> parent = new HashMap<>();
+    HashMap<TreeNode, TreeNode> parent = new HashMap<>();
 
-    // Step 1: Store parent of each node
-    public void addParent(TreeNode root) {
-        if (root == null) return;
+    public void addParent(TreeNode root){
+        if(root == null) return;
 
-        if (root.left != null) {
-            parent.put(root.left, root);
+        if(root.left!=null){
+            parent.put(root.left,root);
             addParent(root.left);
         }
 
-        if (root.right != null) {
-            parent.put(root.right, root);
-            addParent(root.right);
+        if(root.right!=null){
+            parent.put(root.right,root);
+             addParent(root.right);
         }
+
     }
 
-    // Step 2: BFS from target
-    public List<Integer> collectKDistanceNodes(TreeNode target, int k) {
+    public List<Integer> collectKDistanceNodes(TreeNode target,int k){
+
         Queue<TreeNode> queue = new LinkedList<>();
         Set<TreeNode> visited = new HashSet<>();
 
         queue.offer(target);
         visited.add(target);
 
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int size = queue.size();
 
-            if (k == 0) break;
+            if(k == 0) break;
 
             for (int i = 0; i < size; i++) {
                 TreeNode curr = queue.poll();
 
-                // left
-                if (curr.left != null && !visited.contains(curr.left)) {
-                    queue.offer(curr.left);
-                    visited.add(curr.left);
-                }
+             // left
+            if(curr.left!=null && !visited.contains(curr.left)){
+                queue.offer(curr.left);
+                visited.add(curr.left);
+            }
+             
+             //right
+            if(curr.right!=null && !visited.contains(curr.right)){
+                queue.offer(curr.right);
+                visited.add(curr.right);
 
-                // right
-                if (curr.right != null && !visited.contains(curr.right)) {
-                    queue.offer(curr.right);
-                    visited.add(curr.right);
-                }
+            }
 
-                // parent
-                if (parent.containsKey(curr) && !visited.contains(parent.get(curr))) {
-                    queue.offer(parent.get(curr));
-                    visited.add(parent.get(curr));
-                }
+            //parent
+            if(parent.containsKey(curr) && !visited.contains(parent.get(curr))){
+                queue.offer(parent.get(curr));
+                visited.add(parent.get(curr));
+            }
+
             }
             k--;
+
         }
 
         List<Integer> result = new ArrayList<>();
@@ -62,10 +64,14 @@ class Solution {
         }
 
         return result;
+
     }
+
+
 
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         addParent(root);
-        return collectKDistanceNodes(target, k);
+
+        return collectKDistanceNodes(target,k);
     }
 }
