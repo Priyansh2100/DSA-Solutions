a1b58c3dbeb66953128159d5d9768e3948658c5d
Priyansh <150329730+Priyansh2100@users.noreply.github.com>
2026-09-09 23:27:53 +0530

    Time: 39 ms (8.35%), Space: 50.8 MB (5.22%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    static long[][] dp;
+
+    public long coinCount(int i, int[] coins,int amount){
+
+        if(i == coins.length){
+            if(amount == 0) return 0;
+
+            else return Integer.MAX_VALUE;
+        }
+
+        if(dp[i][amount] != -1) {
+            return dp[i][amount];
+        }
+
+        long skip = coinCount(i+1,coins,amount);
+        if(amount-coins[i]<0) return dp[i][amount] = skip;
+
+        long take = 1 + coinCount(i,coins,amount-coins[i]);
+
+        return dp[i][amount] = Math.min(skip,take);
+
+    }
+    public int coinChange(int[] coins, int amount) {
+
+        dp = new long[coins.length][amount + 1];
+
+        for(int i = 0; i < coins.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        int ans = (int)coinCount(0,coins,amount);
+
+        if(ans==Integer.MAX_VALUE) return -1;
+
+        return ans;
+        
+    }
+}
